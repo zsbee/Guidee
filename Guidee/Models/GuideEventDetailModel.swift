1e8d007ca5e16dc3e1c012bb29b4ac200c4c09c7
Zsombor Fuszenecker <zsombor.fuszenecker@skyscanner.net>
2016-11-12 00:34:04 +0100

    fixing my little stupid crash

--- Guidee/Models/GuideEventDetailModel.swift
+++ Guidee/Models/GuideEventDetailModel.swift
@@ -8,22 +8,26 @@ public class GuideEventDetailModel: AnyObject {
     public init(dictionary: NSDictionary) {
         self.title = (dictionary["title"] as! NSString) as String
         self.summary = (dictionary["summary"] as! NSString) as String
-        
-        let carouselModels = dictionary["carouselModels"] as? NSArray as! [NSDictionary]
-        let models:NSMutableArray = NSMutableArray()
-        for carouselModel: NSDictionary in carouselModels {
-            var image: String?
-            var videoId: String?
-            if (carouselModel["imageURL"] as? NSString != nil) {
-                image = (carouselModel["imageURL"] as! NSString) as String
+    
+        if let carouselModels = dictionary["carouselModels"] as? NSArray as? [NSDictionary] {
+            let models:NSMutableArray = NSMutableArray()
+            for carouselModel: NSDictionary in carouselModels {
+                var image: String?
+                var videoId: String?
+                if (carouselModel["imageURL"] as? NSString != nil) {
+                    image = (carouselModel["imageURL"] as! NSString) as String
+                }
+                if (carouselModel["videoYoutubeId"] as? NSString != nil) {
+                    videoId = (carouselModel["videoYoutubeId"] as! NSString) as String
+                }
+                
+                models.add(CarouselItemModel(imageURL: image, videoId: videoId))
             }
-            if (carouselModel["videoYoutubeId"] as? NSString != nil) {
-                videoId = (carouselModel["videoYoutubeId"] as! NSString) as String
-            }
-            
-            models.add(CarouselItemModel(imageURL: image, videoId: videoId))
+            self.carouselModels = models as NSArray as! [CarouselItemModel]
         }
-        self.carouselModels = models as NSArray as! [CarouselItemModel]
+        else {
+            self.carouselModels = [CarouselItemModel]()
+        }
     }
     
     public init(title: String, summary: String, carouselModels: [CarouselItemModel]) {
